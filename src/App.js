46fab3cdc828b8aa6fdcd34f25d03933f38cd970
himanshunagar-tech <72063509+himanshunagar-tech@users.noreply.github.com>
2020-10-26 10:20:50 +0530

    Added why imagesbazaar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import RecatDom from "react-dom";
 import Footer from "./components/Footer";
+import Whyimagesbazaar from "./components/why_imagesbazaar";
 
 function App() {
   return (
+    <div>
     <Footer />
+    <Whyimagesbazaar />
+    </div>
   );
 }
 
