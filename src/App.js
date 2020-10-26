b12d71c47e5c389fd4f065a681c3b718acda506b
Manish-elect <72705839+Manish-elect@users.noreply.github.com>
2020-10-26 10:30:34 +0530

    Update App.js
    remove of import imagesbazzar.jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import RecatDom from "react-dom";
 import Footer from "./components/Footer";
-import Whyimagesbazaar from "./components/why_imagesbazaar";
+
 
 function App() {
   return (
     <div>
     <Footer />
-    <Whyimagesbazaar />
+  
     </div>
   );
 }
